9490187987a394b9352e74c13e6b9edcd004e4da
Moni Shankar <32106437+Moni05@users.noreply.github.com>
2022-03-21 12:59:36 +0530

    Delete src/components directory

--- src/components/Layout/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer(){
-  return (
-    <div className="text-center copy-right">
-      &copy; CRM all right reserved - 2020.
-    </div>
-  );
-};
