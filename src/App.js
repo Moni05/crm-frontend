a5cce0710bbb2f5c23d35dd0cbed20092fe38e85
Moni Shankar <32106437+Moni05@users.noreply.github.com>
2022-03-21 13:04:21 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,16 +8,26 @@ import NewTicket from './pages/NewTicket/NewTicket';
 import TicketListing from './pages/TicketListing/TicketListing';
 import Ticket from './pages/Ticket/Ticket';
 import UserVerification from './pages/UserVerification/UserVerification';
+import { loginSuccess } from "./redux/user/userRedux";
+import { getUserProfile } from "./redux/user/userDetailAction";
 
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
 
-  const { isAuth } = useSelector((state) => state.user);
-  console.log(isAuth);
 
-  useEffect(()=>{},[isAuth]);
+  const dispatch = useDispatch();
+	const { isAuth } = useSelector(state => state.user);
+	const { user } = useSelector(state => state.userDetail);
+  console.log(user._id);
+
+  useEffect(()=>{
+
+    !user._id && dispatch(getUserProfile());
+    !isAuth && JSON.parse(localStorage.getItem("token")).accessToken && dispatch(loginSuccess());
+
+  }, [dispatch, isAuth, user._id])
 
   return (
     <div className="App">
@@ -26,10 +36,10 @@ function App() {
         <Route path="/" element={<IndexPage />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/verify-account/:_id/:name" element={<UserVerification />} />
-        <Route path="/dashboard" element={ isAuth ? <Layout><Dashboard /></Layout> : <Navigate to="/" /> } />
-        <Route path="/create-ticket" element={ isAuth ? <Layout><NewTicket /></Layout> : <Navigate to="/" /> } />
-        <Route path="/tickets" element={ isAuth ? <Layout><TicketListing /></Layout> : <Navigate to="/" /> } />
-        <Route path="/ticket/:id" element={ isAuth ? <Layout><Ticket /></Layout> : <Navigate to="/" /> } />
+        <Route path="/dashboard" element={ user._id ? <Layout><Dashboard /></Layout> : <Navigate to="/" /> } />
+        <Route path="/create-ticket" element={ user._id ? <Layout><NewTicket /></Layout> : <Navigate to="/" /> } />
+        <Route path="/tickets" element={ user._id ? <Layout><TicketListing /></Layout> : <Navigate to="/" /> } />
+        <Route path="/ticket/:id" element={ user._id ? <Layout><Ticket /></Layout> : <Navigate to="/" /> } />
       </Routes>
       </BrowserRouter>
     </div>
