7c317f48769d81b409b09ad3946e3420cd774a9b
Moni Shankar <32106437+Moni05@users.noreply.github.com>
2022-03-30 19:39:58 +0530

    adding router to reset password page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Ticket from './pages/Ticket/Ticket';
 import UserVerification from './pages/UserVerification/UserVerification';
 import { loginSuccess } from "./redux/user/userRedux";
 import { getUserProfile } from "./redux/user/userDetailAction";
+import { PasswordReset } from './pages/PasswordReset/PasswordReset';
 
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
@@ -33,6 +34,7 @@ function App() {
       <Routes>
         <Route path="/" element={<IndexPage />} />
         <Route path="/registration" element={<Registration />} />
+        <Route path="/password-reset" element={<PasswordReset />} />
         <Route path="/verify-account/:_id/:name" element={<UserVerification />} />
         <Route path="/dashboard" element={ isAuth ? <Layout><Dashboard /></Layout> : <Navigate to="/" /> } />
         <Route path="/create-ticket" element={ isAuth ? <Layout><NewTicket /></Layout> : <Navigate to="/" /> } />
