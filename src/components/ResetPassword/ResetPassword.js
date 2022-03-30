80bdb82cacf69b9d20794fd5c993a55cb74b84c3
Moni Shankar <32106437+Moni05@users.noreply.github.com>
2022-03-30 19:48:44 +0530

    updating API call and redux storage logic

--- src/components/ResetPassword/ResetPassword.js
+++ src/components/ResetPassword/ResetPassword.js
@@ -1,32 +1,26 @@
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, Alert, Spinner } from "react-bootstrap";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { sendPasswordResetOtp } from "../../redux/user/userPasswordAction";
 
 
 export default function ResetPassword({formSwitcher}){
 
+    const dispatch = useDispatch();
+
     const [email, setEmail] = useState();
 
-    const handleOnChange = (e) => {
-        const { name, value } = e.target;
-
-        switch (name) {
-			case "email":
-
-				setEmail(value);
-				break;
-
-			default:
-				break;
-		}
-    }
+	const { isLoading, status, message } = useSelector(state => state.password);
 
     const handleOnSubmit = (e) => {
         e.preventDefault();
-
-        if (!email) {
-			return alert("Fill up the email!");
-		}
+        dispatch(sendPasswordResetOtp(email));
     }
+
+    const handleOnChange = e => {
+		const { value } = e.target;
+		setEmail(value);
+	};
 
     return(
         <Container>
@@ -34,6 +28,12 @@ export default function ResetPassword({formSwitcher}){
                 <Col>
                 <h1 className="text-info text-center">Reset Passsword</h1>
                 <br />
+                {message && (
+						<Alert variant={status === "success" ? "success" : "danger"}>
+							{message}
+						</Alert>
+				)}
+                {isLoading && <Spinner variant="primary" animation="border" />}
                 <Form onSubmit={handleOnSubmit}>
                     <Form.Group>
                         <Form.Label>Email</Form.Label>
@@ -45,11 +45,6 @@ export default function ResetPassword({formSwitcher}){
                 <hr />
                 </Col>
             </Row>
-            <Row>
-				<Col>
-					<a href="#" onClick={() => formSwitcher("login")}>Login</a>
-				</Col>
-			</Row>
         </Container>
     )
 }
